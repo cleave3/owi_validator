fac21efe404e0d5d25d756d89c2cebc0bee28ac4
cleave3 <owhiroroeghele@gmail.com>
2020-07-04 11:12:55 +0100

    wrote validation class, wrote test for 78% coverage

---
+++ test/index.js
@@ -0,0 +1,268 @@
+const assert = require("assert");
+const { owi, validate } = require("../validator");
+
+const tests = [
+  {
+    type: "Test for minumum value",
+    errortest: {
+      min: owi(1).min(2).error("number must not be less than 2").exec(),
+      min2: owi("1").min(2).error("number must not be less than 2").exec(),
+    },
+    successtest: {
+      min: owi(2).min(2).error("minimum less than 2").exec(),
+      min: owi("22").min(2).error("minimum less than 2").exec(),
+    },
+    errortype: "Failure test cases for minimum value",
+    successtype: "Success test cases for minimum value",
+    errorlength: 2,
+    error: false,
+    success: true,
+    errormsg: "number must not be less than 2",
+  },
+  {
+    type: "Test for maximum value",
+    errortest: {
+      max: owi(3).max(2).error("number must not be more than 2").exec(),
+      max: owi("333").max(2).error("number must not be more than 2").exec(),
+    },
+    successtest: {
+      max: owi(2).max(2).error("number must not be more than 2").exec(),
+      max: owi("22").max(2).error("number must not be more than 2").exec(),
+    },
+    errortype: "Failure test cases for maximum value",
+    successtype: "Success test cases for maximum value",
+    errorlength: 1,
+    error: false,
+    success: true,
+    errormsg: "number must not be more than 2",
+  },
+  {
+    type: "Test for length of value",
+    errortest: { length: owi(3).length(2).error("length must not be equal 2").exec() },
+    successtest: {
+      length: owi(22).length(2).error("length must not be equal 2").exec(),
+      length2: owi("owhiguy").length(7).error("length must not be equal 7").exec(),
+    },
+    errortype: "Failure test cases for length of value",
+    successtype: "Success test cases for length of value",
+    errorlength: 1,
+    error: false,
+    success: true,
+    errormsg: "length must not be equal 2",
+  },
+  {
+    type: "Test for equality of value",
+    errortest: { equal: owi(3).equal("owhiguy").error("equal must be equal to owhiguy").exec() },
+    successtest: { equal: owi("owhiguy").equal("owhiguy").error("equal must be equal to owhiguy").exec() },
+    errortype: "Failure test cases for for equality of value",
+    successtype: "Success test cases for for equality of value",
+    errorlength: 1,
+    error: false,
+    success: true,
+    errormsg: "equal must be equal to owhiguy",
+  },
+  {
+    type: "Test for param type === string",
+    errortest: { string: owi(3).string().error("expects param to be of type string").exec() },
+    successtest: { string: owi("owhiguy").string().error("expects param to be of type string").exec() },
+    errortype: "Failure test cases for param type === string",
+    successtype: "Success test cases for param type === string",
+    errorlength: 1,
+    error: false,
+    success: true,
+    errormsg: "expects param to be of type string",
+  },
+  {
+    type: "Test for param type === number",
+    errortest: { number: owi("owhiguy").number().error("expects param to be of type number").exec() },
+    successtest: { number: owi(22).number().error("expects param to be of type number").exec() },
+    errortype: "Failure test cases for param type === number",
+    successtype: "Success test cases for param type === number",
+    errorlength: 1,
+    error: false,
+    success: true,
+    errormsg: "expects param to be of type number",
+  },
+  {
+    type: "Test for param type === array",
+    errortest: { array: owi("owhiguy").array().error("expects param to be of type array").exec() },
+    successtest: { array: owi([]).array().error("expects param to be of type array").exec() },
+    errortype: "Failure test cases for param type === array",
+    successtype: "Success test cases for param type === array",
+    errorlength: 1,
+    error: false,
+    success: true,
+    errormsg: "expects param to be of type array",
+  },
+  {
+    type: "Test for param type === email",
+    errortest: {
+      email: owi("owhiguy").email().error("expects param to be of type email").exec(),
+      email2: owi("test@mail.com.co.uk").email().error("expects param to be of type email").exec(),
+      email3: owi("test@mail").email().error("expects param to be of type email").exec(),
+      email4: owi("testmail.co.uk").email().error("expects param to be of type email").exec(),
+      email5: owi("testgmail.com").email().error("expects param to be of type email").exec(),
+    },
+    successtest: {
+      email: owi("owhiroroeghele@gmail.com").email().error("expects param to be of type email").exec(),
+      email2: owi("john@yahoo.com").email().error("expects param to be of type email").exec(),
+      email3: owi("test@mail.com").email().error("expects param to be of type email").exec(),
+      email4: owi("test@mail.co.uk").email().error("expects param to be of type email").exec(),
+    },
+    errortype: "Failure test cases for param type === email",
+    successtype: "Success test cases for param type === email",
+    errorlength: 5,
+    error: false,
+    success: true,
+    errormsg: "expects param to be of type email",
+  },
+  {
+    type: "Test for valid telephone number",
+    errortest: {
+      telephone: owi("owhiguy").telephone().error("expects param to be a valid telephone number").exec(),
+      telephone2: owi("000").telephone().error("expects param to be a valid telephone number").exec(),
+      telephone3: owi("test@mail").telephone().error("expects param to be a valid telephone number").exec(),
+      telephone4: owi(1).telephone().error("expects param to be a valid telephone number").exec(),
+      telephone5: owi("ss7676").telephone().error("expects param to be a valid telephone number").exec(),
+    },
+    successtest: {
+      telephone: owi("09078746744").telephone().error("expects param to be a valid telephone number").exec(),
+      telephone2: owi("+2340898944227").telephone().error("expects param to be a valid telephone number").exec(),
+      telephone3: owi("+1 000 4243 9889").telephone().error("expects param to be a valid telephone number").exec(),
+      telephone4: owi("08165124558").telephone().error("expects param to be a valid telephone number").exec(),
+    },
+    errortype: "Failure test cases for valid telephone number",
+    successtype: "Success test cases for valid telephone number",
+    errorlength: 5,
+    error: false,
+    success: true,
+    errormsg: "expects param to be a valid telephone number",
+  },
+  {
+    type: "Test for param type date",
+    errortest: {
+      date: owi("owhiguy").date().error("expects param to be a valid date").exec(),
+      date2: owi("01").date().error("expects param to be a valid date").exec(),
+    },
+    successtest: {
+      date3: owi("04-06-2020").date().error("expects param to be a valid date").exec(),
+      date4: owi("04/06/2020").date().error("expects param to be a valid date").exec(),
+    },
+    errortype: "Failure test cases for param type date",
+    successtype: "Success test cases for param type date",
+    errorlength: 2,
+    error: false,
+    success: true,
+    errormsg: "expects param to be a valid date",
+  },
+  {
+    type: "Test for param type === boolean",
+    errortest: {
+      boolean: owi("owhiguy").boolean().error("expects param to be of type boolean").exec(),
+      boolean2: owi(1).boolean().error("expects param to be of type boolean").exec(),
+      boolean3: owi(0).boolean().error("expects param to be of type boolean").exec(),
+    },
+    successtest: {
+      boolean: owi(true).boolean().error("expects param to be of type boolean").exec(),
+      boolean2: owi(false).boolean().error("expects param to be of type boolean").exec(),
+    },
+    errortype: "Failure test cases for param type === boolean",
+    successtype: "Success test cases for param type === boolean",
+    errorlength: 3,
+    error: false,
+    success: true,
+    errormsg: "expects param to be of type boolean",
+  },
+  {
+    type: "Test for optional field",
+    errortest: {
+      optional: owi("owhiguy").boolean().optional().error("expects param to be of type boolean").exec(),
+    },
+    successtest: {
+      optional: owi().boolean().optional().error("expects param to be of type boolean").exec(),
+    },
+    errortype: "Failure test cases for optional field",
+    successtype: "Success test cases for optional field",
+    errorlength: 1,
+    error: false,
+    success: true,
+    errormsg: "expects param to be of type boolean",
+  },
+  {
+    type: "Test for undefined",
+    errortest: {
+      required: owi().number().string().min().max().boolean().email().array().length().optional().date().regex().card().telephone().equal().required().error().exec(),
+    },
+    successtest: {
+      required: owi("testmode").required().error("param is required").exec(),
+    },
+    errortype: "Failure test cases for required field",
+    successtype: "Success test cases for required field",
+    errorlength: 1,
+    error: false,
+    success: true,
+    errormsg: "value is required",
+  },
+  {
+    type: "Test for required field",
+    errortest: {
+      required: owi().required().error("param is required").exec(),
+    },
+    successtest: {
+      required: owi("testmode").required().error("param is required").exec(),
+    },
+    errortype: "Failure test cases for required field",
+    successtype: "Success test cases for required field",
+    errorlength: 1,
+    error: false,
+    success: true,
+    errormsg: "param is required",
+  },
+  //   {
+  //     type: "Test for regex pattern",
+  //     errortest: {
+  //       regex: owi("A").regex(/[a-z]/).error("expects param to be lower case").exec(),
+  //       regex: owi("A").regex().error("Please provide a regex pattern").exec(),
+  //     },
+  //     successtest: {
+  //       regex: owi("ghghg").regex(/[a-z]/).error("expects param to be lower case").exec(),
+  //       regex: owi("ATYTY").regex(/[A-Z]/).error("expects param to be upper case").exec(),
+  //       regex: owi(565).regex(/[\d+]/).error("expects param to be a number").exec(),
+  //     },
+  //     errortype: "Failure test cases for regex pattern",
+  //     successtype: "Success test cases for regex pattern",
+  //     errorlength: 1,
+  //     error: false,
+  //     success: true,
+  //     errormsg: "Please provide a regex pattern",
+  //   },
+];
+
+describe("owi-validator", () => {
+  tests.map((test, i) => {
+    describe(test.type, () => {
+      describe(test.errortype, () => {
+        it("validity test", () => {
+          assert.equal(validate(test.errortest).isValid, test.error);
+        });
+        it("Error Length test", () => {
+          assert.equal(validate(test.errortest).errors.length, test.errorlength);
+        });
+        it("Error message test", () => {
+          assert.equal(validate(test.errortest).errors[0], test.errormsg);
+        });
+      });
+      describe(test.successtype, () => {
+        it("validity test", () => {
+          assert.equal(validate(test.successtest).isValid, test.success);
+        });
+        it("Error Length test", () => {
+          assert.equal(validate(test.successtest).errors.length, 0);
+        });
+        it("Error message test", () => {
+          assert.equal(validate(test.successtest).errors.join(), "");
+        });
+      });
+    });
+  });
+});
---
+++ validator/index.js
@@ -0,0 +1,176 @@
+class Validator {
+  constructor(param) {
+    this.param = param;
+    this.paramtitle = "value";
+    this.errors = null;
+    this.patterns = {
+      email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+      telephone: /[+]?\d[ ]?[(]?\d{3}[)]?[ ]?\d{2,3}[- ]?\d{2}[- ]?\d{2}/,
+      electron: /^(4026|417500|4405|4508|4844|4913|4917)\d+$/,
+      maestro: /^(5018|5020|5038|5612|5893|6304|6759|6761|6762|6763|0604|6390)\d+$/,
+      dankort: /^(5019)\d+$/,
+      interpayment: /^(636)\d+$/,
+      unionpay: /^(62|88)\d+$/,
+      visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+      master: /^5[1-5][0-9]{14}$/,
+      amex: /^3[47][0-9]{13}$/,
+      diners: /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/,
+      discover: /^6(?:011|5[0-9]{2})[0-9]{12}$/,
+      jcb: /^(?:2131|1800|35\d{3})\d{11}$/,
+      date: /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/,
+    };
+  }
+
+  luhnCheck(val) {
+    var sum = 0;
+    for (var i = 0; i < val.length; i++) {
+      var intVal = parseInt(val.substr(i, 1));
+      if (i % 2 == 0) {
+        intVal *= 2;
+        if (intVal > 9) {
+          intVal = 1 + (intVal % 10);
+        }
+      }
+      sum += intVal;
+    }
+    return sum % 10 == 0;
+  }
+
+  length(length) {
+    if (this.param === undefined) return this;
+    const p = this.param.toString();
+    if (p.trim() === "" || p.length !== length) this.errors = `${this.paramtitle} must be ${length} characters long`;
+    return this;
+  }
+
+  equal(param) {
+    if (this.param === undefined) return this;
+    if (this.param !== param) this.errors = `${this.paramtitle} must be equal to ${param}`;
+    return this;
+  }
+
+  min(minvalue) {
+    let errormsg = "";
+    if (this.param === undefined) return this;
+    if (typeof this.param === "string" && this.param.length < minvalue) errormsg += `${this.paramtitle} must not be less than ${minvalue} characters`;
+    if (typeof this.param === "number" && this.param < minvalue) errormsg += `${this.paramtitle} must not be less than ${minvalue}`;
+    errormsg !== "" ? (this.errors = errormsg) : null;
+    return this;
+  }
+
+  max(maxvalue) {
+    let errormsg = "";
+    if (this.param === undefined) return this;
+    if (typeof this.param === "string" && this.param.length > maxvalue) errormsg += `${this.paramtitle} must not be greater than ${maxvalue} characters`;
+    if (typeof this.param === "number" && this.param > maxvalue) errormsg += `${this.paramtitle} must not be greater than ${maxvalue}`;
+    errormsg !== "" ? (this.errors = errormsg) : null;
+    return this;
+  }
+
+  string() {
+    if (this.param === undefined) return this;
+    if (typeof this.param !== "string" || this.param.trim() === "") this.errors = `${this.paramtitle} must be of type string`;
+    return this;
+  }
+
+  number() {
+    if (this.param === undefined) return this;
+    if (typeof this.param !== "number") this.errors = `${this.paramtitle} must be of type number`;
+    return this;
+  }
+
+  array() {
+    if (this.param === undefined) return this;
+    if (!Array.isArray(this.param)) this.errors = `${this.paramtitle} must be of type array`;
+    return this;
+  }
+
+  email() {
+    if (this.param === undefined) return this;
+    if (!this.patterns.email.test(this.param.trim())) this.errors = `${this.paramtitle} is not valid`;
+    return this;
+  }
+
+  telephone() {
+    if (this.param === undefined) return this;
+    this.param = this.param.toString().replace(/" "/g, "");
+    if (!this.patterns.telephone.test(this.param)) this.errors = `${this.paramtitle} is not a valid telephone number`;
+    return this;
+  }
+
+  card(card) {
+    let errormsg = "";
+    if (this.param === undefined) return this;
+    if (card === undefined) errormsg += "Please specify a card type";
+    if (!this.patterns[card].test(this.param) && !this.luhnCheck(this.param)) errormsg += `${this.paramtitle} is not a valid ${card}card`;
+    this.errors = errormsg;
+    return this;
+  }
+
+  date() {
+    if (this.param === undefined) return this;
+    if (!this.patterns.date.test(this.param)) this.errors = `${this.paramtitle} is not a valid date`;
+    return this;
+  }
+
+  boolean() {
+    if (this.param === undefined) return this;
+    if (typeof this.param !== "boolean") this.errors = `${this.paramtitle} must be of type boolean`;
+    return this;
+  }
+
+  regex(regex) {
+    let errormsg = "";
+    if (this.param === undefined) return this;
+    if (regex === undefined) {
+      errormsg += "Please provide a regex pattern";
+      this.errors = errormsg;
+      return this;
+    }
+    console.log(`"reg test" ${this.param}, ${this.errors}`, regex.test(this.param));
+    if (regex.test(this.param) === false) errormsg += `${this.paramtitle} does not match ${regex}`;
+    this.errors = errormsg;
+    return this;
+  }
+
+  error(errormsg) {
+    errormsg === undefined || errormsg.trim() === "" || this.errors === null ? null : (this.errors = errormsg);
+    return this;
+  }
+
+  optional() {
+    if (this.param === undefined) this.errors = null;
+    return this;
+  }
+
+  required() {
+    if (this.param === undefined) this.errors = `${this.paramtitle} is required`;
+    return this;
+  }
+
+  exec() {
+    return this.errors !== null ? this.errors : true;
+  }
+}
+
+/**
+ * Init validation class
+ * @param {any} param - body parameter
+ * @returns {object} - validator object
+ */
+const owi = (param) => new Validator(param);
+
+/**
+ *
+ * @param {object} schema - schema object
+ * @returns {object} - response object
+ */
+const validate = (schema) => {
+  let errors = [];
+  for (entity in schema) {
+    if (schema[entity] !== true && schema[entity] !== null) errors.push(schema[entity]);
+  }
+  return errors.length > 0 ? { isValid: false, errors } : { isValid: true, errors };
+};
+
+module.exports = { owi, validate };
